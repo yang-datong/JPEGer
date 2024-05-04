295f5fda09447e8e6f3b657ef50b0c87d63cfa85
yangjing <546229768@qq.com>
2024-05-05 04:51:39 +0800

    添加BMP.cpp文件，为Encode 做准备

---
+++ BMP.cpp
@@ -0,0 +1,176 @@
+#include "BMP.hpp"
+#include <cmath>
+#include <cstdint>
+#include <ios>
+
+File::BMP::BMP() {}
+File::BMP::~BMP() { close(); }
+
+int File::BMP::BMPToRBG(string &bmpFilePath, string rgbFilePath) {
+  ifstream bmp_file(bmpFilePath, ios::binary);
+  if (!bmp_file.is_open()) {
+    std::cerr << "Error opening BMP file!" << std::endl;
+    return -1;
+  }
+  // 读取文件头
+  bmp_file.read((char *)&head, sizeof(head));
+  // 读取信息头
+  bmp_file.read((char *)&info, sizeof(info));
+
+  _width = info.width;
+  _height = info.height;
+
+  std::cout << "Width: " << _width << std::endl;
+  std::cout << "Height: " << _height << std::endl;
+
+  // 为像素数据分配内存
+  int bufferSize = _width * _height * 3;
+  uint8_t *buffer = new uint8_t[bufferSize];
+  uint8_t *re_buffer = new uint8_t[bufferSize];
+
+  // 读取RGB数据
+  bmp_file.seekg(head.offset, std::ios_base::beg);
+  bmp_file.read((char *)buffer, bufferSize);
+
+  for (int i = 0; i < bufferSize; i++) {
+    re_buffer[i] = buffer[(bufferSize - 1 - i)];
+  }
+  delete[] buffer;
+  buffer = nullptr;
+
+  _rgbBuffer = re_buffer;
+  _rgbBufferSize = bufferSize;
+  bmp_file.close();
+
+  /* 不写文件则保留内存，等手动释放 */
+  if (rgbFilePath.empty())
+    return 0;
+
+  ofstream outputFile(rgbFilePath, ios_base::binary);
+  if (!outputFile.is_open()) {
+    std::cerr << "Error opening RGG file!" << std::endl;
+    return -1;
+  }
+  outputFile.write((const char *)_rgbBuffer, _rgbBufferSize);
+  outputFile.close();
+  std::cout << "Output RGB file: " + rgbFilePath << std::endl;
+
+  /* 写文件则释放内存 */
+  close();
+  return 0;
+}
+
+int File::BMP::BMPToYUV(string &bmpFilePath, string yuvFilePath) {
+  BMPToRBG(bmpFilePath);
+  RGBToYUV();
+  if (yuvFilePath.empty())
+    return 0;
+
+  ofstream outputFile(yuvFilePath, ios_base::binary);
+  if (!outputFile.is_open()) {
+    std::cerr << "Error opening RGG file!" << std::endl;
+    return -1;
+  }
+  outputFile.write((const char *)_Y, _YUVSize);
+  outputFile.write((const char *)_U, _YUVSize);
+  outputFile.write((const char *)_V, _YUVSize);
+  outputFile.close();
+  std::cout << "Output RGB file: " + yuvFilePath << std::endl;
+
+  /* 写文件则释放内存 */
+  close();
+  return 0;
+}
+
+int File::BMP::RGBToYUV() {
+  _YUVSize = _height * _width;
+  _Y = new uint8_t[_YUVSize];
+  _U = new uint8_t[_YUVSize];
+  _V = new uint8_t[_YUVSize];
+  int offset = -1;
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      offset = i * _height + j;
+      uint8_t r = _rgbBuffer[offset * 3];
+      uint8_t g = _rgbBuffer[offset * 3 + 1];
+      uint8_t b = _rgbBuffer[offset * 3 + 2];
+
+      _Y[offset] = round(0.299f * r + 0.587f * g + 0.114f * b);
+      _U[offset] = round(-0.1687f * r - 0.3313f * g + 0.5f * b + 128);
+      _V[offset] = round(0.5f * r - 0.4187f * g - 0.0813f * b + 128);
+    }
+  }
+  return 0;
+}
+
+int File::BMP::RGBToBMP(string &rgbFilePath, int w, int h,
+                        string &bmpFilePath) {
+  printf("head byte -> %ld\n", sizeof(Head));
+  printf("info byte -> %ld\n", sizeof(Info));
+  FILE *fp_rgb24 = fopen(rgbFilePath.c_str(), "rb");
+  FILE *fp_bmp = fopen(bmpFilePath.c_str(), "wb+");
+
+  if (fp_rgb24 == NULL || fp_bmp == NULL) {
+    printf("NULL!");
+    return (-1);
+  }
+
+  unsigned char *rgb24_buf = (unsigned char *)malloc(w * h * 3);
+  unsigned char *re_rgb24_buf = (unsigned char *)malloc(w * h * 3);
+  fread(re_rgb24_buf, 1, w * h * 3, fp_rgb24);
+
+  //倒置字节
+  for (int i = 0; i < w * h * 3; ++i) {
+    rgb24_buf[i] = re_rgb24_buf[((w * h * 3) - 1) - i];
+  }
+
+  int head_size = sizeof(Head) + sizeof(Info);
+  printf("%d\n", head_size);
+  head.file_size = head_size + w * h * 3;
+  head.offset = head_size;
+
+  info.info_size = sizeof(info);
+  info.width = w;
+  info.height = h;
+  info.num_color_planes = 1;
+  info.bit = 3 * 8; /* rgb24 */
+  info.compress_type = 0;
+  info.kernel_data_size = w * h * 3;
+  info.horizontal_resolution = 0;
+  info.vertical_resolution = 0;
+  info.tab_index = 0;
+  info.important_idx = 0;
+
+  fwrite(&head, 1, sizeof(head), fp_bmp);
+  fwrite(&info, 1, sizeof(info), fp_bmp);
+  fwrite(rgb24_buf, 1, w * h * 3, fp_bmp);
+
+  free(re_rgb24_buf);
+  re_rgb24_buf = NULL; // uaf
+  free(rgb24_buf);
+  rgb24_buf = NULL;
+  fclose(fp_rgb24);
+  fclose(fp_bmp);
+  return 0;
+}
+
+int File::BMP::close() {
+  if (_rgbBuffer) {
+    delete[] _rgbBuffer;
+    _rgbBuffer = nullptr;
+  }
+  _rgbBufferSize = 0;
+  if (_Y) {
+    delete[] _Y;
+    _Y = nullptr;
+  }
+  if (_U) {
+    delete[] _U;
+    _U = nullptr;
+  }
+  if (_V) {
+    delete[] _V;
+    _V = nullptr;
+  }
+  return 0;
+}
---
+++ include/BMP.hpp
@@ -0,0 +1,64 @@
+#ifndef BMP_HPP_4ZTJJIQB
+#define BMP_HPP_4ZTJJIQB
+
+#include "Type.hpp"
+#include <cstdint>
+
+namespace File {
+#pragma pack(push, 1)
+typedef struct _Head {
+  uint16_t magic{0x4D42};
+  int32_t file_size{0};
+  int32_t extension{0};
+  int32_t offset{0};
+} Head;
+
+typedef struct _Info {
+  int32_t info_size{0};
+  int32_t width{0}; /* unit pixel */
+  int32_t height{0};
+  unsigned short num_color_planes{0};
+  unsigned short bit{0};
+  int32_t compress_type{0};
+  int32_t kernel_data_size{0};
+  int32_t horizontal_resolution{0};
+  short vertical_resolution{0};
+  /* 2byte下面需要再补充一个，如果置为0的话也可以不用写，编译器会通过字节对齐的方式跳过2个字节(下面是int_32)*/
+  char _vertical_resolution{0}; /* 补充1byte */
+  int32_t tab_index{0};
+  int32_t important_idx{0};
+} Info;
+#pragma pack(pop)
+
+class BMP {
+ private:
+  Head head;
+  Info info;
+  int _width = 0;
+  int _height = 0;
+  uint8_t *_rgbBuffer = nullptr;
+  int _rgbBufferSize = 0;
+  uint8_t *_Y = nullptr, *_U = nullptr, *_V = nullptr;
+  int _YUVSize = 0;
+
+ public:
+  BMP();
+  ~BMP();
+  uint8_t *getRGBBuffer() { return _rgbBuffer; }
+  int getRGBBufferSize() { return _rgbBufferSize; }
+  uint8_t *getY() { return _Y; }
+  uint8_t *getU() { return _U; }
+  uint8_t *getV() { return _V; }
+  int getYUVSize() { return _YUVSize; }
+
+  int RGBToYUV();
+
+  int RGBToBMP(string &rgbFilePath, int width, int height, string &bmpFilePath);
+
+  int BMPToRBG(string &bmpFilePath, string rgbFilePath = "");
+  int BMPToYUV(string &bmpFilePath, string yuvFilePath = "");
+  int close();
+};
+
+} // namespace File
+#endif /* end of include guard: BMP_HPP_4ZTJJIQB */
